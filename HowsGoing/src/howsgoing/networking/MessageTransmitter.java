336c69713c8a9469d4ee2450b6dbb0657d3429ae
Erdem Akbaba <engineer.akbaba@gmail.com>
2019-01-31 21:10:59 +0200

    This project is based on Java socket programming

---
+++ HowsGoing/src/howsgoing/networking/MessageTransmitter.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package howsgoing.networking;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hp_
+ */
+public class MessageTransmitter extends Thread
+{
+    String message;
+    String hostname;
+    int port;
+    WritableGUI gui;
+    
+    public MessageTransmitter()
+    {
+        
+    }
+    
+    public MessageTransmitter(String message, String hostname, int port)
+    {
+        this.message = message;
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    @Override
+    public void run()
+    {
+        if (port == 21 || port == 80 || port == 8888 || port == 88)
+        {
+            gui.write("Please use an unreserved port!");
+        }
+        else
+        {
+            try
+            {
+                Socket s = new Socket(hostname, port);
+                s.getOutputStream().write(message.getBytes());
+                s.close();
+            }
+            catch (IOException ex)
+            {
+                Logger.getLogger(MessageTransmitter.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+    }
+    
+}
